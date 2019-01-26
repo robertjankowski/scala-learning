084fe98acab043f1912bed0e0fbd4ba9edd22e55
robertjankowski <robjanw11@wp.pl>
2019-01-26 22:32:43 +0100

    more rdd basics functions

--- sparkTutorial/src/main/scala/com/spark/tutorial/RDDBasics.scala
+++ sparkTutorial/src/main/scala/com/spark/tutorial/RDDBasics.scala
@@ -20,7 +20,7 @@ object RDDBasics extends App {
     sqrt(sqrt(x))
   }
 
-  val numbers = (1 to 1000000).toList.map(e => e.toDouble)
+  val numbers = (1 to 100).toList.map(e => e.toDouble)
   val rddSingle = sc.parallelize(numbers)
   val rddMultiple = sc.parallelize(numbers, 2) // split to 2 pieces
 
@@ -33,5 +33,41 @@ object RDDBasics extends App {
   val toSqrtMultiple = rddMultiple.map(doubleSqrt).collect
   val duration2 = (System.nanoTime - t2) / 1e9d
   println(s"Duration multiple: ${duration2}, ${toSqrtMultiple.head}") // faster options !!
+
+  // flatMap
+  val words = List("Hello", "World", "How are you ?")
+  val wordSplit = sc.parallelize(words).flatMap(word => word.split(" "))
+  wordSplit.collect().foreach { println }
+
+  // average of numbers
+  val nums = (1 to 100).toList
+  val result = sc
+    .parallelize(nums)
+    .aggregate((0, 0))(
+      (acc, value) => (acc._1 + value, acc._2 + 1),
+      (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
+    )
+  val avg = result._1 / result._2.toDouble
+  println(s"Average = ${avg}")
+
+  val rddNums = sc.parallelize(nums)
+  // top
+  val topNums = rddNums.top(10)
+  println("Top numbers:")
+  topNums.foreach { println }
+
+  // takeOrdered
+  val takeOrdered = rddNums.takeOrdered(5)(Ordering[Int].reverse)
+  println("Take ordered:")
+  takeOrdered.foreach { println }
+
+  // takeSample
+  val takeSample = rddNums.takeSample(withReplacement = false, 10)
+  println("Take sample without replacement:")
+  takeSample.foreach { println }
+
+  // reduce = sum
+  val sumNums = rddNums.reduce(_ + _)
+  println(s"Sum of 1-100 = ${sumNums}")
 
 }
