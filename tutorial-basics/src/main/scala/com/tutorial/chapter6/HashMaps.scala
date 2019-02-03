09d0a7179492a59c6a906e3f8ce24f3c33a0e519
robertjankowski <robjanw11@wp.pl>
2019-02-03 16:44:38 +0100

    hash and tree map

---
+++ tutorial-basics/src/main/scala/com/tutorial/chapter6/HashMaps.scala
@@ -0,0 +1,30 @@
+package com.tutorial.chapter6
+
+import scala.collection.immutable.HashMap
+
+object HashMaps extends App {
+
+  val hashMap1 = HashMap[String, Int]("Mike" -> 12,
+                                      "Tom" -> 10,
+                                      "Jerry" -> 20,
+                                      "Marry" -> 99)
+  hashMap1.foreach { map =>
+    println(s"Key: ${map._1}, value: ${map._2}")
+  }
+
+  // add element
+  val hashMap2 = hashMap1 + ("Jake" -> 29)
+
+  // get difference between maps
+  val diffs1 = (hashMap2.toSet.diff(hashMap1.toSet)).toMap
+  diffs1.foreach { println }
+  println()
+
+  // remove HashMap
+  val hashMap3 = hashMap2 -- Seq("Tom", "Marry")
+  hashMap3.foreach { println(_) }
+
+  // empty map
+  val empty = HashMap.empty[String, Int]
+
+}
---
+++ tutorial-basics/src/main/scala/com/tutorial/chapter6/TreeMaps.scala
@@ -0,0 +1,30 @@
+package com.tutorial.chapter6
+
+import scala.collection.immutable.TreeMap
+
+object TreeMaps extends App {
+
+  type Drink = (Int, String)
+  val treeMap =
+    TreeMap[String, Drink]("Rob" -> (10, "Tea"), "Bob" -> (20, "Coffee"))
+
+  treeMap.foreach { println }
+
+  // access element
+  println(s"Rob drink: ${treeMap("Rob")}")
+
+  // add element
+  val treeMap1 = treeMap + ("Jessi" -> (10, "Bob"))
+
+  object AlphabetOrdering extends Ordering[String] {
+    override def compare(x: String, y: String): Int = {
+      x.compareTo(y)
+    }
+  }
+
+  val treeDiffOrder =
+    TreeMap[String, String]("c" -> "C", "b" -> "B", "f" -> "f")(
+      AlphabetOrdering)
+  treeDiffOrder.foreach { println }
+
+}
